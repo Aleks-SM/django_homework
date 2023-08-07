e9c2628d7c2349955fd3c07a119fea80058aa0de
AleksSM <aleks-m83@mail.ru>
2023-08-07 20:50:29 +0400

    add read csv

--- 1.2-requests-templates/pagination/stations/views.py
+++ 1.2-requests-templates/pagination/stations/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render, redirect
 from django.urls import reverse
+from django.core.paginator import Paginator
 import csv
+
 
 def index(request):
     return redirect(reverse('bus_stations'))
@@ -12,23 +14,28 @@ def read_csv():
         tmp = list(rows)
     bus_station = {}
     for i in tmp:
-        # print("name:{} street:{} district:{}".format(i[1], i[4], i[6]))
+        print("name:{} street:{} district:{}".format(i[1], i[4], i[6]))
         bus_station[i[8]] = {'name': i[1], 'street': i[4], 'district': i[6]}
-
     return bus_station
 
-def bus_stations(request):
+def pagi(request):
+    paginator = Paginator([str(i) for i in range(100)], 15)
+    page = paginator.get_page(5)
+    context = {'page': page}
+    return render(request, 'stations/index.html', context)
+
+def get_bus_stations(request):
     # получите текущую страницу и передайте ее в контекст
     # также передайте в контекст список станций на странице
-    # print(read_csv())
-    # < td > {{station.Name}} < / td >
-    # < td > {{station.Street}} < / td >
-    # < td > {{station.District}} < / td >
-    # context = {
-    # #     'bus_stations': ...,
-    # #     'page': ...,
-    # }
-    context = read_csv()
-    # print(context.get('ДК Зуева'))
-    print(context.keys())
+    csvFilename = 'data-398-2018-08-30.csv'
+    with open(csvFilename, 'r', encoding='utf-8', newline='') as csvfile:
+        reader = csv.DictReader(csvfile, delimiter=',')
+        bus_stations = list(reader)
+        page_number = int(request.GET.get('page', 1))
+        paginator = Paginator(bus_stations, 5)
+        page = paginator.get_page(page_number)
+        context = {
+             'bus_stations': bus_stations,
+             'page': page,
+        }
     return render(request, 'stations/index.html', context)
