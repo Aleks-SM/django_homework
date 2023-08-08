ddfb3d32cd569cd5cd056e88dce865b9b34b2ab4
AleksSM <aleks-m83@mail.ru>
2023-08-08 19:46:43 +0400

    add pagination

--- 1.2-requests-templates/pagination/stations/views.py
+++ 1.2-requests-templates/pagination/stations/views.py
@@ -10,27 +10,23 @@ def index(request):
     return redirect(reverse('bus_stations'))
 
 def read_csv():
-    csvFilename = BUS_STATION_CSV
-    with open(csvFilename, encoding='utf-8') as csvfile:
+    with open(BUS_STATION_CSV, encoding='utf-8') as csvfile:
         rows = csv.reader(csvfile, delimiter=",")
         tmp = list(rows)
-    bus_station = {}
+    bus_station = []
     for i in tmp:
-        bus_station[i[8]] = {'name': i[1],
-                             'street': i[4],
-                             'district': i[6]
-                             }
+        tmp = {'Name': i[1],
+               'Street': i[4],
+               'District': i[6]
+               }
+        bus_station.append(tmp)
     return bus_station
 
 def bus_stations(request):
     # получите текущую страницу и передайте ее в контекст
     # также передайте в контекст список станций на странице
 
-    with open(BUS_STATION_CSV, 'r', encoding='utf-8', newline='') as csvfile:
-        reader = csv.DictReader(csvfile, delimiter=',')
-
-        stations = list(reader)
-    paginator = Paginator(stations, 5)
+    paginator = Paginator(list(read_csv()), 5)
     page_number = int(request.GET.get('page', 1))
     page = paginator.get_page(page_number)
 
