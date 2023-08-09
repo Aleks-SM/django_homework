dbbf60d4c3edcf70142b6ef3216559b9fd61d8c9
AleksSM <aleks-m83@mail.ru>
2023-08-09 20:55:30 +0400

    add model

--- 2.1-databases/work_with_database/phones/models.py
+++ 2.1-databases/work_with_database/phones/models.py
@@ -3,4 +3,11 @@ from django.db import models
 
 class Phone(models.Model):
     # TODO: Добавьте требуемые поля
+    id = models.AutoField
+    name = models.CharField(max_length=30)
+    price = models.DecimalField(max_digits=6, decimal_places=2)
+    image = models.ImageField()
+    release_date = models.DateField()
+    lte_exists = models.BooleanField()
+    slug
     pass
