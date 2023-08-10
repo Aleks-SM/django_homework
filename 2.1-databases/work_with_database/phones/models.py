d2692aa653d78aa70a4812bf9f4f2bb068209583
AleksSM <aleks-m83@mail.ru>
2023-08-10 20:07:49 +0400

    update models

--- 2.1-databases/work_with_database/phones/models.py
+++ 2.1-databases/work_with_database/phones/models.py
@@ -5,9 +5,9 @@ class Phone(models.Model):
     # TODO: Добавьте требуемые поля
     id = models.AutoField
     name = models.CharField(max_length=30)
-    price = models.DecimalField(max_digits=6, decimal_places=2)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
     image = models.ImageField()
     release_date = models.DateField()
     lte_exists = models.BooleanField()
-    slug
-    pass
+    slug = models.SlugField(max_length=30, unique=True)
+
