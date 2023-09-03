9b0fd7d73b71e77b8b705ac14c815d23c490e93a
AleksSM <aleks-m83@mail.ru>
2023-09-03 18:02:33 +0400

    change models

--- 2.2-databases-2/m2m-relations/articles/models.py
+++ 2.2-databases-2/m2m-relations/articles/models.py
@@ -7,10 +7,33 @@ class Article(models.Model):
     text = models.TextField(verbose_name='Текст')
     published_at = models.DateTimeField(verbose_name='Дата публикации')
     image = models.ImageField(null=True, blank=True, verbose_name='Изображение',)
+    # tag = models.ManyToManyField('Tag', through='Scope')
 
     class Meta:
         verbose_name = 'Статья'
         verbose_name_plural = 'Статьи'
+        ordering = ['-published_at']
 
     def __str__(self):
         return self.title
+
+class Tag(models.Model):
+    name = models.CharField(max_length=50, blank=True)
+
+    name_articles = models.ManyToManyField(Article, related_name='name_articles', through='Scope')
+
+    class Meta:
+        verbose_name = 'Раздел'
+        verbose_name_plural = 'Разделы'
+
+    def __str__(self):
+        return self.name
+
+
+class Scope(models.Model):
+    is_main = models.BooleanField(verbose_name='Основной')
+    tag = models.ForeignKey(Tag, on_delete=models.CASCADE, related_name='tag', verbose_name='Раздел')
+    article = models.ForeignKey(Article, on_delete=models.CASCADE, related_name='scopes')
+
+    class Meta:
+        ordering = ['-is_main', 'tag']
