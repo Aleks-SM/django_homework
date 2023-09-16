9ede109b87b7d60bfe52e20ae9500d59e19b4b67
AleksSM <aleks-m83@mail.ru>
2023-09-16 09:36:33 +0400

    change models.py

--- 3.2-crud/stocks_products/logistic/models.py
+++ 3.2-crud/stocks_products/logistic/models.py
@@ -3,18 +3,29 @@ from django.db import models
 
 
 class Product(models.Model):
-    title = models.CharField(max_length=60, unique=True)
-    description = models.TextField(null=True, blank=True)
+    title = models.CharField(max_length=60, unique=True, verbose_name='Наименование')
+    description = models.TextField(null=True, blank=True, verbose_name='Описание')
 
+    def __str__(self):
+        return self.title
+    class Meta:
+        verbose_name = 'Товар'
+        verbose_name_plural = 'Товары'
 
 class Stock(models.Model):
-    address = models.CharField(max_length=200, unique=True)
+    address = models.CharField(max_length=200, unique=True, verbose_name='Адресс склада')
     products = models.ManyToManyField(
         Product,
         through='StockProduct',
         related_name='stocks',
     )
 
+    def __str__(self):
+        return self.address
+
+    class Meta:
+        verbose_name = 'Склад'
+        verbose_name_plural = 'Склады'
 
 class StockProduct(models.Model):
     stock = models.ForeignKey(
