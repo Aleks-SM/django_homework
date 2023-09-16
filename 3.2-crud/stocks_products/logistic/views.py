675d5230d58ffacca63f576094d3419e8d8df372
AleksSM <aleks-m83@mail.ru>
2023-09-16 09:04:00 +0400

    add filters in views.py

--- 3.2-crud/stocks_products/logistic/views.py
+++ 3.2-crud/stocks_products/logistic/views.py
@@ -1,16 +1,22 @@
+from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework.viewsets import ModelViewSet
+from rest_framework.filters import SearchFilter
 
 from logistic.models import Product, Stock
 from logistic.serializers import ProductSerializer, StockSerializer
 
 
+
 class ProductViewSet(ModelViewSet):
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
-    # при необходимости добавьте параметры фильтрации
+    filter_backends = [SearchFilter]
+    search_field = ['title', 'description']
 
 
 class StockViewSet(ModelViewSet):
     queryset = Stock.objects.all()
     serializer_class = StockSerializer
-    # при необходимости добавьте параметры фильтрации
+    filter_backends = [SearchFilter, DjangoFilterBackend]
+    filterset_fields = ['products']
+    search_field = ['products__title', 'products__description']
