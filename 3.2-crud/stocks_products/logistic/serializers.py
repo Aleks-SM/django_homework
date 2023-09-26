2130e77e8f59bf4f1abb54aa98640b1cb96048f5
AleksSM <aleks-m83@mail.ru>
2023-09-26 18:49:37 +0400

    change serializers.py

--- 3.2-crud/stocks_products/logistic/serializers.py
+++ 3.2-crud/stocks_products/logistic/serializers.py
@@ -1,28 +1,29 @@
 from rest_framework import serializers
 from logistic.models import Product, Stock, StockProduct
 
+
 class ProductSerializer(serializers.ModelSerializer):
     class Meta:
         model = Product
-        fields = ['id', 'title', 'description']
+        fields = ["id", "title", "description"]
 
 
 class ProductPositionSerializer(serializers.ModelSerializer):
     class Meta:
         model = StockProduct
-        fields = ['id', 'quantity', 'price', 'product']
-
+        fields = ["id", "quantity", "price", "product"]
 
 
 class StockSerializer(serializers.ModelSerializer):
     positions = ProductPositionSerializer(many=True)
+
     class Meta:
         model = Stock
-        fields = ['id', 'address', 'products']
+        fields = ["id", "address", "products"]
 
     def create(self, validated_data):
         # достаем связанные данные для других таблиц
-        positions = validated_data.pop('positions')
+        positions = validated_data.pop("positions")
 
         # создаем склад по его параметрам
         stock = super().create(validated_data)
@@ -31,12 +32,14 @@ class StockSerializer(serializers.ModelSerializer):
         # в нашем случае: таблицу StockProduct
         # с помощью списка positions
         for position in positions:
-            StockProduct.objects.create(stock=stock, product=product, quantity=quantity, price=price)
+            StockProduct.objects.create(
+                stock=stock, product=product, quantity=quantity, price=price
+            )
         return stock
 
     def update(self, instance, validated_data):
         # достаем связанные данные для других таблиц
-        positions = validated_data.pop('positions')
+        positions = validated_data.pop("positions")
 
         # обновляем склад по его параметрам
         stock = super().update(instance, validated_data)
@@ -45,6 +48,9 @@ class StockSerializer(serializers.ModelSerializer):
         # в нашем случае: таблицу StockProduct
         # с помощью списка positions
         for position in positions:
-            StockProduct.objects.update_or_create(stock=stock, product=product, defaults={'price': price, 'quantity': quantity})
-        return stock
+            StockProduct.objects.update_or_create(
+                stock=stock,
+                product=product,
+                defaults={"price": price, "quantity": quantity},
+            )
         return stock
